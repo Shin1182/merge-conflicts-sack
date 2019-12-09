31fd0f4d6b21d791914bcf3b08ad5ace1f8655c8
Shin1182 <53378615+Shin1182@users.noreply.github.com>
2019-12-09 21:31:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello, flatter world!";
-=======
-var truth = "Hello, Falt Earth!";
->>>>>>> 8325216a7b86111b893dc08825d889e430cd56e4
+
+var truth = "Hello, GitHub!";
+
